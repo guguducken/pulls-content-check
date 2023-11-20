fa4642f0a12b878edf6b76d9d95c30928303d1ad
guguducken <endeavorjia@gmail.com>
2023-11-20 15:01:51 +0800

    temp finished

---
+++ src/index.js
@@ -0,0 +1,109 @@
+import core from '@actions/core';
+import github from '@actions/github';
+import chalk from 'chalk';
+import { fromMarkdown } from 'mdast-util-from-markdown';
+import { toString } from 'mdast-util-to-string'
+
+const githubToken = core.getInput("github_token", { required: true });
+const oc = github.getOctokit(githubToken);
+
+async function main() {
+    const titleIssue = core.getInput("title_for_find_issue");
+    const titleContent = core.getInput("title_for_find_content");
+
+    const pullContent = github.context.payload?.pull_request?.body;
+    if (pullContent === undefined || pullContent.length == 0) {
+        core.setFailed(`this request is not pull_request or the body of this pull_request is empty`);
+    }
+
+    let issueIsValid = true
+    let contentIsValid = true
+
+    const tree = fromMarkdown(pullContent)
+
+    for (let i = 0; i < tree.children.length; i++) {
+        const content = toString(tree.children[i]);
+        let inputContent = "";
+        switch (content) {
+            case titleIssue:
+                // i+1 to skip title
+                i, inputContent = drumpToNextHeading(tree, i + 1)
+                issueIsValid = await checkIssueValid(inputContent);
+                continue;
+            case titleContent:
+                i, inputContent = drumpToNextHeading(tree, i + 1)
+                contentIsValid = checkContentValid(inputContent);
+                continue;
+            default:
+                continue;
+        }
+    }
+
+    core.setOutput("pull_valid", `${issueIsValid || contentIsValid}`)
+}
+
+async function checkIssueValid(issueContent) {
+    // check issue in this repo
+    let regSlef = /#[0-9]+/igm
+    let result = issueContent.match(regSlef)
+    if (result !== null && result.length != 0) {
+        // check weather issue number is valid
+        for (let i = 0; i < result.length; i++) {
+            // remove prefix #
+            const issue = result[i].substring(1);
+            console.log("start check issue " + chalk.red(`${issue}`) + " in this repo " + chalk.greenBright(`${github.context.repo.owner}/${github.context.repo.repo}`));
+            const { data: data, status: status } = await oc.rest.issues.get({
+                ...github.context.repo,
+                issue_number: issue
+            });
+            if (status == 200 && data.pull_request === undefined) {
+                console.log("issue " + chalk.red(`${issue}`) + " in this repo " + chalk.greenBright(`${github.context.repo.owner}/${github.context.repo.repo}`) + " is valid, so return true")
+                return true
+            }
+
+        }
+    }
+
+    // check issue in other repo
+    const regOther = /https:\/\/github.com\/([a-zA-Z0-9\-_\.]+)\/([a-zA-Z0-9\-_\.]+)\/issues\/(\d+)/igm;
+    let resultOtherRepo = issueContent.matchAll(regOther)
+
+    let otherIssue = resultOtherRepo.next();
+    let haveNext = !otherIssue.done;
+    while (haveNext) {
+        console.log(`start check other repo issue ` + chalk.greenBright(otherIssue.value[0]))
+        const { data: data, status: status } = await oc.rest.issues.get({
+            owner: otherIssue.value[1],
+            repo: otherIssue.value[2],
+            issue_number: otherIssue.value[3]
+        });
+        if (status == 200 && data.pull_request === undefined) {
+            console.log("issue " + chalk.red(`${issue}`) + " in other repo " + chalk.greenBright(`${ otherIssue.value[1]}/${ otherIssue.value[2]}`) + " is valid, so return true")
+            return true
+        }
+        otherIssue = resultOtherRepo.next();
+        haveNext = !otherIssue.done;
+    }
+    console.log(chalk.red("there is no valid issue, so return false"));
+    return false
+}
+
+function drumpToNextHeading(tree, ind) {
+    let content = toString(tree.children[ind]);
+    while (ind + 1 < tree.children.length && tree.children[ind + 1].type != "heading") {
+        content += " " + toString(tree.children[++ind]);
+    }
+    return ind, content;
+}
+
+function checkContentValid(messageContent) {
+    console.log("pull message is: " + chalk.greenBright(messageContent));
+    messageContent = messageContent.replace("debug", "").replace("fix", "").replace(/[!"#$%&'()*+,-./:;<=>?@\[\]\^_`{|}~ \\]/igm, "")
+    console.log(messageContent.length)
+    if (messageContent.length >= 3) {
+        return true
+    }
+    return false
+}
+
+main();
